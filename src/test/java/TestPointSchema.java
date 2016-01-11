ea46cccd65e4a218df89ae2e2b39857e030d1c35
TheolZacharopoulos <theol.zacharopoulos@gmail.com>
2016-01-11 17:15:08 +0100

    finish up test schema tests

--- src/test/java/TestPointSchema.java
+++ src/test/java/TestPointSchema.java
@@ -16,7 +16,6 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 
 import static org.junit.Assert.*;
@@ -127,6 +126,22 @@ public class TestPointSchema {
                 endPointField = field;
         }
 
+        Klass pointType = null;
+        for (Type type : pointSchema.types()) {
+            if (type.name().equals("Point"))
+                pointType = (Klass) type;
+        }
+
+        Field xField = null;
+        Field yField = null;
+        for (Field field : pointType.fields()) {
+            if (field.name().equals("x"))
+                xField = field;
+
+            if (field.name().equals("y"))
+                yField = field;
+        }
+
         assertNotNull(startPointField);
         assertThat(startPointField.name(), is("startPoint"));
         assertThat(startPointField.optional(), is(false));
@@ -138,7 +153,7 @@ public class TestPointSchema {
                     pointSchema,
                     Collections.emptySet(),
                     Collections.emptySet(),
-                    new LinkedHashSet<Field>(Arrays.asList(startPointField, endPointField))))
+                    new LinkedHashSet<>(Arrays.asList(xField, yField))))
         );
 
         assertNotNull(endPointField);
@@ -152,24 +167,77 @@ public class TestPointSchema {
                     pointSchema,
                     Collections.emptySet(),
                     Collections.emptySet(),
-                    new LinkedHashSet<>(Arrays.asList(startPointField, endPointField))))
+                    new LinkedHashSet<>(Arrays.asList(xField, yField))))
         );
     }
 
     @Test
-    public void testEmptyIntegerInitialization() {
+    public void testEmptyPointInitialization() {
         Point point = pointFactory.point();
         assertThat(point.x(), is(0));
         assertThat(point.y(), is(0));
     }
 
     @Test
-    public void testNonEmptyIntegerInitialization() {
+    public void testNonEmptyPointInitialization() {
         final Integer x = 1;
         final Integer y = 2;
 
         Point point = pointFactory.point(x, y);
         assertEquals(point.x(), x);
         assertEquals(point.y(), y);
+    }
+
+    @Test
+    public void testEmptyLineInitialization() {
+        Line line = pointFactory.line();
+        assertNull(line.startPoint());
+        assertNull(line.endPoint());
+    }
+
+    @Test(expected=RuntimeException.class)
+    public void testNonEmptyLineInitialization_nonMObject() {
+        final Point startPoint = new Point() {
+            @Override
+            public Integer x(Integer... x) {
+                return 1;
+            }
+
+            @Override
+            public Integer y(Integer... y) {
+                return 2;
+            }
+        };
+
+        final Point endPoint = new Point() {
+            @Override
+            public Integer x(Integer... x) {
+                return 3;
+            }
+
+            @Override
+            public Integer y(Integer... y) {
+                return 4;
+            }
+        };
+
+        Line line = pointFactory.line(startPoint, endPoint);
+        assertEquals(line.startPoint().x(), new Integer(1));
+        assertEquals(line.startPoint().y(), new Integer(2));
+        assertEquals(line.endPoint().x(), new Integer(3));
+        assertEquals(line.endPoint().y(), new Integer(4));
+    }
+
+    @Test
+    public void testNonEmptyLineInitialization() {
+        final Point startPoint = pointFactory.point(1, 2);
+
+        final Point endPoint = pointFactory.point(3, 4);
+
+        Line line = pointFactory.line(startPoint, endPoint);
+        assertEquals(line.startPoint().x(), new Integer(1));
+        assertEquals(line.startPoint().y(), new Integer(2));
+        assertEquals(line.endPoint().x(), new Integer(3));
+        assertEquals(line.endPoint().y(), new Integer(4));
     }
 }
