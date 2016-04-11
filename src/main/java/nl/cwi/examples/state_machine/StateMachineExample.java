b5cd731f119deca5e19ba7552b89e8cbf70aea19
TheolZacharopoulos <theol.zacharopoulos@gmail.com>
2016-04-11 16:36:14 +0200

    State machine example

--- src/main/java/nl/cwi/examples/state_machine/StateMachineExample.java
+++ src/main/java/nl/cwi/examples/state_machine/StateMachineExample.java
@@ -3,6 +3,9 @@ package nl.cwi.examples.state_machine;
 import nl.cwi.examples.state_machine.schemas.Machine;
 import nl.cwi.examples.state_machine.schemas.State;
 import nl.cwi.examples.state_machine.schemas.Transition;
+import nl.cwi.managed_data_4j.ccconcerns.aspects.UpdateLogger;
+import nl.cwi.managed_data_4j.ccconcerns.patterns.observer.Observable;
+import nl.cwi.managed_data_4j.ccconcerns.patterns.observer.ObservableFactory;
 import nl.cwi.managed_data_4j.framework.SchemaFactoryProvider;
 import nl.cwi.managed_data_4j.language.data_manager.BasicFactory;
 import nl.cwi.managed_data_4j.language.schema.boot.SchemaFactory;
@@ -10,48 +13,92 @@ import nl.cwi.managed_data_4j.language.schema.load.SchemaLoader;
 import nl.cwi.managed_data_4j.language.schema.models.definition.Schema;
 
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 
 public class StateMachineExample {
+
+    public final static String OPEN_STATE       = "Open";
+    public final static String CLOSED_STATE     = "Closed";
+    public final static String OPEN_TRANSITION  = "open";
+    public final static String CLOSE_TRANSITION = "close";
 
     public static void main(String[] args) {
 
         final Schema schemaSchema = SchemaFactoryProvider.getSchemaSchema();
         final SchemaFactory schemaFactory = SchemaFactoryProvider.getSchemaFactory();
 
-        final Schema stateMachineSchema = SchemaLoader.load(schemaFactory, schemaSchema, State.class, Transition.class, Machine.class);
+        final Schema stateMachineSchema =
+                SchemaLoader.load(schemaFactory, schemaSchema, State.class, Transition.class, Machine.class);
         final BasicFactory basicFactoryForStateMachines = new BasicFactory(StateMachineFactory.class, stateMachineSchema);
         final StateMachineFactory stateMachineFactory = basicFactoryForStateMachines.make();
 
-        State open = stateMachineFactory.State();
-        open.name("Open");
+//        // ================================ Update Logging ===================================
+//        System.out.println("=============");
+//        System.out.println("Observable Objects: ");
+//
+//        final ObservableFactory observableFactory = new ObservableFactory(StateMachineFactory.class, stateMachineSchema);
+//        final StateMachineFactory observableStateMachineFactory = observableFactory.make();
+//
+//        final State openState = observableStateMachineFactory.State();
+//
+//        // Add Logging concerns
+//        ((Observable) openState).observe(UpdateLogger::log);
+//        // ===================================================================================
 
-        State closed = stateMachineFactory.State();
-        closed.name("Closed");
+        final State openState = stateMachineFactory.State();
 
-        Transition closeTransition = stateMachineFactory.Transition();
-        closeTransition.event("close");
-        closeTransition.from(open, closed);
+        openState.name(OPEN_STATE);
 
-        Transition openTransition = stateMachineFactory.Transition();
-        openTransition.event("open");
-        openTransition.from(closed, open);
+        final State closedState = stateMachineFactory.State();
+        closedState.name(CLOSED_STATE);
 
-        Machine doorStateMachine = stateMachineFactory.Machine();
-        doorStateMachine.start(open);
-        doorStateMachine.states(new HashSet<>(Arrays.asList(open, closed)).toArray(new State[2]));
+        final Transition closeTransition = stateMachineFactory.Transition();
+        closeTransition.event(CLOSE_TRANSITION);
+        closeTransition.from(openState);
+        closeTransition.to(closedState);
 
-//        interpretStateMachine(doorStateMachine);
+        final Transition openTransition = stateMachineFactory.Transition();
+        openTransition.event(OPEN_TRANSITION);
+        openTransition.from(closedState);
+        openTransition.to(openState);
+
+        openState.in(openTransition);
+        openState.out(closeTransition);
+
+        closedState.in(closeTransition);
+        closedState.out(openTransition);
+
+        final Machine doorStateMachine = stateMachineFactory.Machine();
+        doorStateMachine.start(openState);
+        doorStateMachine.states(openState, closedState);
+
+        final List<String> commands = new LinkedList<>(Arrays.asList(
+                OPEN_TRANSITION,
+                CLOSE_TRANSITION,
+                OPEN_TRANSITION,
+                CLOSE_TRANSITION,
+                OPEN_TRANSITION));
+
+        interpretStateMachine(doorStateMachine, commands);
     }
 
     private static void interpretStateMachine(Machine sm, List<String> commands) {
+
         State current = sm.start();
+        System.out.println("Start: " + current.name());
 
-        System.out.println(current.name());
+        for (String event : commands) {
+            Transition transition = current.out().stream()
+                .filter(trans -> trans.event().equals(event))
+                .findFirst()
+                .orElse(null);
 
-        for (String command : commands) {
-
+            if (transition != null) {
+                current = transition.to();
+                System.out.println(
+                    "Changed: " + transition.from().name() + " to " + transition.to().name() + ", current: " + current.name());
+            }
         }
     }
 }
--- src/main/java/nl/cwi/examples/state_machine/schemas/Transition.java
+++ src/main/java/nl/cwi/examples/state_machine/schemas/Transition.java
@@ -2,8 +2,11 @@ package nl.cwi.examples.state_machine.schemas;
 
 import nl.cwi.managed_data_4j.language.schema.models.definition.M;
 import nl.cwi.managed_data_4j.language.schema.models.definition.annotations.Inverse;
+import nl.cwi.managed_data_4j.language.schema.models.definition.annotations.Key;
 
 public interface Transition extends M {
+
+    @Key
     String event(String... event);
 
     @Inverse(other = State.class, field = "out")
